1b792226238694475e77d2885c34765ecddd8f21
Tomasz Jeznach <tjeznach@rivosinc.com>
2022-12-08 23:21:48 -0800

    docs: riscv-iommu simple how-to

---
+++ riscv-iommu/README.md
@@ -0,0 +1,76 @@
+# RISC-V IOMMU Experiment
+
+## Dependencies
+
+Install tools and dependencies required for each project:
+
+- QEMU: https://github.com/tjeznach/qemu/tree/tjeznach/riscv-iommu
+- LINUX: https://github.com/tjeznach/linux/tree/tjeznach/riscv-iommu-aia
+- CROSVM: https://github.com/tjeznach/crosvm/tree/tjeznach/topic/riscv-iommu
+
+## Installation
+
+Run simple script to fetch, build and run experiment.
+
+```bash
+curl --proto '=https' --tlsv1.2 -sSf https://github.com/tjeznach/docs/blob/master/riscv-iommu/run-qemu.sh | bash
+```
+
+## RISC-V Host
+
+```
+$ echo "$(</sys/firmware/devicetree/base/model)"
+riscv-virtio,qemu
+
+$ cat /proc/cpuinfo
+processor	: 0
+hart		: 0
+isa		: rv64imafdch_smaia_ssaia_sstc_zihintpause
+mmu		: sv57
+mvendorid	: 0x0
+marchid		: 0x7015e
+mimpid		: 0x7015e
+
+processor	: 1
+hart		: 1
+isa		: rv64imafdch_smaia_ssaia_sstc_zihintpause
+mmu		: sv57
+mvendorid	: 0x0
+marchid		: 0x7015e
+mimpid		: 0x7015e
+
+
+$ ls /sys/bus/pci/devices/0000\:00\:0?.0/iommu -l
+lrwxrwxrwx 1 root root 0 Dec  9 06:45 /sys/bus/pci/devices/0000:00:03.0/iommu -> ../../../../../virtual/iommu/riscv-iommu@40000c000
+lrwxrwxrwx 1 root root 0 Dec  9 06:45 /sys/bus/pci/devices/0000:00:04.0/iommu -> ../../../../../virtual/iommu/riscv-iommu@40000c000
+lrwxrwxrwx 1 root root 0 Dec  9 06:45 /sys/bus/pci/devices/0000:00:07.0/iommu -> ../../../../../virtual/iommu/riscv-iommu@40000c000
+lrwxrwxrwx 1 root root 0 Dec  9 06:45 /sys/bus/pci/devices/0000:00:09.0/iommu -> ../../../../../virtual/iommu/riscv-iommu@40000c000
+
+$ ls /sys/bus/pci/devices/0000\:00\:0?.0/iommu_group -l
+lrwxrwxrwx 1 root root 0 Dec  9 06:45 /sys/bus/pci/devices/0000:00:03.0/iommu_group -> ../../../../../../kernel/iommu_groups/0
+lrwxrwxrwx 1 root root 0 Dec  9 06:45 /sys/bus/pci/devices/0000:00:04.0/iommu_group -> ../../../../../../kernel/iommu_groups/1
+lrwxrwxrwx 1 root root 0 Dec  9 06:45 /sys/bus/pci/devices/0000:00:07.0/iommu_group -> ../../../../../../kernel/iommu_groups/3
+lrwxrwxrwx 1 root root 0 Dec  9 06:45 /sys/bus/pci/devices/0000:00:09.0/iommu_group -> ../../../../../../kernel/iommu_groups/2
+
+```
+
+Attach NVMe1n1 to VFIO-PCI
+
+```
+$ export BDF="0000:00:04.0"
+$ export DID="1b36 0010"
+$ echo "$BDF" > /sys/bus/pci/devices/$BDF/driver/unbind
+$ echo "$DID" > /sys/bus/pci/drivers/vfio-pci/new_id
+
+$ ls /dev/vfio -l
+crw------- 1 root root 246,   0 Dec  9 06:44 1
+crw-rw-rw- 1 root root  10, 196 Dec  9 06:43 vfio
+```
+
+Run CrosVM using NVMe1n1 as direct attached root drive.
+
+```
+$ crosvm --no-syslog run --disable-sandbox -p 'nokaslr console=ttyS0 root=/dev/nvme0n1' --vfio "/sys/bus/pci/devices/$BDF" /usr/share/Image
+```
+
+
---
+++ riscv-iommu/run-qemu.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+set -p
+
+TOP=`realpath .`
+
+fetch_all() {
+    cd "${TOP}"
+    if [[ ! -d "${TOP}/crosvm/.git" ]]; then
+        git clone git@github.com:tjeznach/crosvm.git
+        cd crosvm
+        git checkout tjeznach/topic/riscv-iommu 
+        git submodule update --init
+        cd ..
+    fi
+    if [[ ! -d "${TOP}/linux/.git" ]]; then
+        git clone https://github.com/tjeznach/linux.git
+        cd linux
+        git checkout tjeznach/riscv-iommu-aia
+        cd ..
+    fi
+    if [[ ! -d "${TOP}/qemu/.git" ]]; then
+        git clone https://github.com/tjeznach/qemu.git
+        cd qemu
+        git checkout tjeznach/riscv-iommu
+        git submodule update --init
+        cd ..
+    fi
+}
+
+
+# Get CrosVM and build riscv64 target
+build_qemu() {
+    cd "${TOP}/qemu"
+    mkdir build && cd build
+    ../configure --target-list="riscv64-softmmu"
+    make -j$(nproc)
+    cd "${TOP}/qemu"
+    make -C roms/opensbi CROSS_COMPILE=riscv64-linux-gnu- O=../../build PLATFORM_RISCV_XLEN=64 PLATFORM=generic -j $(nproc)
+}
+
+build_kernel() {
+    cd "${TOP}/linux"
+    mkdir build
+    make ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- KBUILD_DEFCONFIG=iommu_virt_defconfig O=build -j$(nproc) defconfig Image
+}
+
+build_crosvm() {
+    cd "${TOP}/crosvm"
+    export PKG_CONFIG_ALLOW_CROSS="true"
+    export CROS_RUST=1
+    cargo build --no-default-features --target=riscv64gc-unknown-linux-gnu --release
+}
+
+build_rootfs() {
+    REL="kinetic"
+    cd "${TOP}"
+    if [[ ! -f "${REL}.img" ]]; then
+        truncate -s 1G $REL.img
+        mkfs.ext4 $REL.img
+        mkdir -p $REL
+        sudo mount -oloop $REL.img $REL
+        sudo debootstrap --arch=riscv64 $REL $REL
+        sudo sed 's/root:.:/root::/' -i $REL/etc/shadow
+        sudo echo 'riscv-guest' > $REL/etc/hostname
+        sudo umount $REL
+        cp $REL.img nvme1.img
+        sudo mount -oloop $REL.img $REL
+        sudo cp crosvm/target/riscv64gc-unknown-linux-gnu/release/crosvm $REL/usr/bin
+        sudo cp linux/build/arch/riscv/boot/Image $REL/usr/share/Image
+        sudo echo 'riscv-host' > $REL/etc/hostname
+        sudo umount $REL
+        cp $REL.img nvme0.img
+    fi
+}
+
+fetch_all
+
+build_qemu
+build_kernel
+build_crosvm
+build_rootfs
+
+# run QEMU
+
+cd "${TOP}"
+
+QEMU="qemu/build/qemu-system-riscv64"
+KERNEL="linux/build/arch/riscv/boot/Image"
+OPEN_SBI="qemu/build/platform/generic/firmware/fw_jump.elf "
+
+NVME0="nvme0.img,format=raw"
+NVME1="nvme1.img,format=raw"
+# release/Fedora-Developer-37-20221130.n.0-nvme.raw.img,format=raw"
+
+test -x "${QEMU}" || die "Can't find QEMU: ${QEMU}"
+test -x "${KERNEL}" || die "Can't find KERNEL: ${KERNEL}"
+test -x "${OPEN_SBI}" || die "Can't find OPEN_SBI: ${OPEN_SBI}"
+
+QARGS=""
+# machine definition
+QARGS="${QARGS} -no-reboot -no-user-config -nographic -machine virt,aia=aplic-imsic,aia-guests=4 -cpu rv64 -smp 2"
+QARGS="${QARGS} -m 4G -object memory-backend-file,id=sysmem,mem-path=/dev/shm/4g,size=4G,share=on"
+
+# emulation backends
+QARGS="${QARGS} -drive file=${NVME0},read-only=off,id=nvme0"
+QARGS="${QARGS} -drive file=${NVME1},read-only=off,id=nvme1" 
+QARGS="${QARGS} -netdev user,id=host-net,hostfwd=tcp::2223-:23"
+QARGS="${QARGS} -netdev user,id=guest-net,hostfwd=tcp::4423-:23"
+# host devices
+QARGS="${QARGS} -device x-riscv-iommu-pci,addr=1.0"
+QARGS="${QARGS} -device nvme,serial=10101010,drive=nvme0,addr=3.0"
+QARGS="${QARGS} -device virtio-net-pci,romfile=,netdev=host-net,disable-legacy=on,disable-modern=off,iommu_platform=on,ats=on,addr=7.0"
+# guest devices
+QARGS="${QARGS} -device nvme,serial=87654321,drive=nvme1,addr=4.0"
+QARGS="${QARGS} -device e1000,romfile=,netdev=host-net,netdev=guest-net,addr=9.0"
+
+# kernel arguments
+KARGS="nokaslr earlycon=sbi console=ttyS0 root=/dev/nvme0n1 ro"
+
+# run qemu
+${QEMU} -bios ${OPEN_SBI} -append "${KARGS}" -kernel ${KERNEL} ${QARGS}
+
